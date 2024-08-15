3787d6b25bb0fda95bd45effef38cc85ea56a306
Neo 🗿🍷 <165345955+NeKosmico@users.noreply.github.com>
2024-08-14 21:41:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -70,6 +70,41 @@ app.get('/ytdl', async (req, res) => {
   }
 });
 
+app.get('/ytdlv', async (req, res) => {
+  const { q, apikey } = req.query;
+  if (!q || apikey !== "sicuani") {
+    return res.status(400).json({ status: false, respuesta: "Faltan parámetros" });
+  }
+
+  try {
+    const videoUrl = q;
+    if (!ytdl.validateURL(videoUrl)) {
+      return res.status(400).send("Youtube! >:v");
+    }
+
+    // Asegúrate de definir o eliminar `cookies` si no se necesita
+    const agent = ytdl.createProxyAgent({ uri: myProxy });
+    const { formats, videoDetails } = await ytdl.getInfo(videoUrl, { agent });
+
+    const quality = ['360p', '480p'];
+    let audioFormat = null;
+
+    quality.some(qlv => {
+      audioFormat = formats.find(f => f.mimeType?.includes("video/mp4") && f.qualityLabel === qlv && f.hasVideo && f.hasAudio);
+      return audioFormat; // `.some` devolverá `true` si se encuentra `audioFormat`
+    });
+
+    if (!audioFormat) {
+      return res.status(400).send("Formato de audio no encontrado");
+    }
+
+    res.json(audioFormat);
+    
+  } catch (e) {
+    res.json({ error: e.toString() });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
